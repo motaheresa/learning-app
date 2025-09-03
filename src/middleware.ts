018d168d238d15be47e0af21ce3a8b5acab6bc2f
MOHAMED TAHER <119441809+motaheresa@users.noreply.github.com>
2025-09-03 08:51:25 +0300

    Initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
+  
   const session = req.cookies.get("session_user");
-  if (!session && (req.nextUrl.pathname.startsWith("/dashboard")||req.nextUrl.pathname=="/")) {
+
+  // Protect dashboard and homepage
+  if (!session && (req.nextUrl.pathname.startsWith("/admin") || req.nextUrl.pathname === "/")) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
-}
 
+  return NextResponse.next(); // <-- important to continue if no redirect
+}
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/:path*", "/"], 
 };
